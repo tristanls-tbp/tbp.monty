d44521f19c3194f0a16ca6ff609d7e2c29a06475
Tristan Slominski <tslominski@thousandbrains.org>
2025-12-30 11:50:35 -0600

    refactor!: use actual episode number to retrieve eval stats

--- src/tbp/monty/frameworks/run_parallel.py
+++ src/tbp/monty/frameworks/run_parallel.py
@@ -405,11 +405,11 @@ def single_evaluate(experiment):
             # `self.use_parallel_wandb_logging` set to True
             # This way, the logger does not flush its buffer in the
             # `exp.run()` call above.
-            return get_episode_stats(exp, "eval")
+            return get_episode_stats(exp, "eval", exp.config.episode)
 
 
-def get_episode_stats(exp, mode):
-    eval_stats = exp.monty_logger.get_formatted_overall_stats(mode, 0)
+def get_episode_stats(exp, mode, episode: int = 0):
+    eval_stats = exp.monty_logger.get_formatted_overall_stats(mode, episode)
     exp.monty_logger.flush()
     # Remove overall stats field since they are only averaged over 1 episode
     # and might cause confusion.
