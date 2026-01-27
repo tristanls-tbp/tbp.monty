452131867f91e81f18f9d63ce03a4c16ee06d38c
Ramy Mounir <44840015+ramyamounir@users.noreply.github.com>
2026-01-26 19:21:20 -0500

    fix: set individual ts to "None" in no_reset experiments (#718)

--- src/tbp/monty/frameworks/models/no_reset_evidence_matching.py
+++ src/tbp/monty/frameworks/models/no_reset_evidence_matching.py
@@ -80,6 +80,7 @@ class MontyForNoResetEvidenceGraphMatching(MontyForEvidenceGraphMatching):
         self._is_done = False
         self.reset_episode_steps()
         self.switch_to_matching_step()
+        self._reset_terminal_states()
 
         # keep target up-to-date for logging
         self.primary_target = primary_target
@@ -90,6 +91,10 @@ class MontyForNoResetEvidenceGraphMatching(MontyForEvidenceGraphMatching):
 
         # reset LMs and SMs buffers to save memory
         self._reset_modules_buffers()
+
+    def _reset_terminal_states(self):
+        for lm in self.learning_modules:
+            lm.set_individual_ts(None)
 
     def _reset_modules_buffers(self):
         """Resets buffers for LMs and SMs."""
