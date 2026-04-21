fix!: update MMW environment interface classes to use quat tuple for rotation (#939)

src/tbp/monty/frameworks/environments/embodied_data.py
@@ -15,7 +15,6 @@ from pprint import pformat
 from typing import Mapping, Sequence, cast
 
 import numpy as np
-import quaternion as qt
 from omegaconf import ListConfig
 
 from tbp.monty.frameworks.actions.actions import (
@@ -494,7 +493,7 @@ class OmniglotEnvironmentInterface(EnvironmentInterfacePerObject):
         self.current_object = idx
         self.primary_target = {
             "object": self.object_names[idx],
-            "rotation": qt.quaternion(0, 0, 0, 1),
+            "rotation": (0.0, 0.0, 0.0, 1.0),
             "euler_rotation": np.array([0, 0, 0]),
             "quat_rotation": [0, 0, 0, 1],
             "position": np.array([0, 0, 0]),
@@ -592,7 +591,7 @@ class SaccadeOnImageEnvironmentInterface(EnvironmentInterfacePerObject):
         target_object_formatted = "_".join(target_object.split("_")[1:])
         self.primary_target = {
             "object": target_object_formatted,
-            "rotation": qt.quaternion(0, 0, 0, 1),
+            "rotation": (0.0, 0.0, 0.0, 1.0),
             "euler_rotation": np.array([0, 0, 0]),
             "quat_rotation": [0, 0, 0, 1],
             "position": np.array([0, 0, 0]),
@@ -667,7 +666,7 @@ class SaccadeOnImageFromStreamEnvironmentInterface(SaccadeOnImageEnvironmentInte
         # targets corresponding to the current scene ?
         self.primary_target = {
             "object": "no_label",
-            "rotation": qt.quaternion(0, 0, 0, 1),
+            "rotation": (0.0, 0.0, 0.0, 1.0),
             "euler_rotation": np.array([0, 0, 0]),
             "quat_rotation": [0, 0, 0, 1],
             "position": np.array([0, 0, 0]),