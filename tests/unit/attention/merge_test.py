709c9400e964d3b6b8267b5e64e2343abeb8d1e9
Tristan Slominski <tslominski@thousandbrains.org>
2026-09-18 11:38:46 -0500

    test: merge test scaffolding

--- tests/unit/attention/merge_test.py
+++ tests/unit/attention/merge_test.py
@@ -11,10 +11,15 @@ from __future__ import annotations
 import unittest
 
 from hypothesis import given
+from hypothesis import strategies as st
 
 from tbp.monty.attention.merge import Union
 from tbp.monty.attention.voxel_grid import VoxelGrid
 from tests.unit.attention import strategies
+
+
+# @st.composite
+# def disjoint_voxels(draw: st.DrawFn) -> list[Voxel]:
 
 
 class UnionTest(unittest.TestCase):
@@ -28,3 +33,21 @@ class UnionTest(unittest.TestCase):
             self.union(grid_a, grid_b)
 
     # disjoint, full overlap, partial overlap
+    @given(grid=strategies.default_voxel_grid())
+    def test_returns_grid_size_which_is_the_sum_of_the_input_grid_sizes_when_input_grids_are_disjoint(  # noqa: E501
+        self, grid: VoxelGrid
+    ):
+        pass
+
+    def test_returns_grid_b_when_input_grids_fully_overlap(self):
+        pass
+
+    def test_returns_grid_size_less_than_the_sum_of_the_input_grid_sizes_when_input_grids_partially_overlap(  # noqa: E501
+        self,
+    ):
+        pass
+
+    def test_grid_b_weights_survive_in_overlapping_voxels_when_input_grids_overlap(
+        self,
+    ):
+        pass
